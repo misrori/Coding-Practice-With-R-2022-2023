9c35f5b5bd059ab43d5763e0e5f2c945da197974
misrori <ormraat.pte@gmail.com>
2022-11-06 09:35:25 +0100

    week-7

--- week-7/week-7-functions.R
+++ week-7/week-7-functions.R
@@ -1,4 +1,4 @@
-
+a<-1
 greet_person_external <- function(greeting = "Hello", name = "World") {
   print(paste0(greeting, ", ", name, "!!!"))
 }
@@ -9,3 +9,13 @@ greet_person_random <- function(greeting, name) {
   print(paste0(sample(greeting,1), ", " , sample(name,1 ), "!"))
 }
 
+greet_person_external('hehe', 'jo')
+
+df <- iris
+
+names(df)[1:4]
+
+for (i in names(df)[1:4]) {
+  class(df[[i]])
+  df[[i]] <- df[[i]]*2
+}
